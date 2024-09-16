5e32d51440c9c841d5dcd86ac6933d5ef3987c16
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-09-16 19:35:38 +0300

    .

--- lib/google_maps_widget.dart
+++ lib/google_maps_widget.dart
@@ -15,6 +15,8 @@ late GoogleMapController mapController;
 class _GoogleMapsWidgetState extends State<GoogleMapsWidget> {
   String mapStyle = '';
   Set<Marker> markers = {};
+  Set<Polyline> polyLines = {};
+  Set<Polygon> polyGons = {};
   @override
   void initState() {
     super.initState();
@@ -25,14 +27,19 @@ class _GoogleMapsWidgetState extends State<GoogleMapsWidget> {
         mapStyle = style;
       });
     });
-    initMarkers();
+    // initMarkers();
+    // initPolylines();
+    initPolygons();
   }
 
   @override
   Widget build(BuildContext context) {
     return Stack(children: [
       GoogleMap(
-        markers: markers,
+        polygons: polyGons,
+        polylines: polyLines,
+        zoomControlsEnabled: false,
+        // markers: markers,
         //mapType: MapType.satellite,
         style: mapStyle,
         onMapCreated: (controller) {
@@ -40,35 +47,67 @@ class _GoogleMapsWidgetState extends State<GoogleMapsWidget> {
           // initMapStyle();
         },
         initialCameraPosition: const CameraPosition(
-          target: LatLng(30.560073234352902, 31.1513466305802),
-          zoom: 10,
+          target: LatLng(30.109712694237142, 31.5106095812379),
+          zoom: 6,
         ),
       ),
-      Positioned(
-          bottom: 20,
-          left: 16,
-          right: 16,
-          child: FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  mapController.animateCamera(CameraUpdate.newLatLng(
-                      const LatLng(31.56049009629473, 25.27466807111593)));
-                });
-              },
-              child: const Text("Change Location")))
+      // Positioned(
+      //     bottom: 20,
+      //     left: 16,
+      //     right: 16,
+      //     child: FloatingActionButton(
+      //         onPressed: () {
+      //           setState(() {
+      //             mapController.animateCamera(CameraUpdate.newLatLng(
+      //                 const LatLng(31.56049009629473, 25.27466807111593)));
+      //           });
+      //         },
+      //         child: const Text("Change Location")))
     ]);
   }
 
-  void initMarkers()async {
-    var customIcon= await BitmapDescriptor.asset(ImageConfiguration(
-      size: Size(30, 30)
-    ),"assets/images/location.png" );
+  void initMarkers() async {
+    var customIcon = await BitmapDescriptor.asset(
+        const ImageConfiguration(size: Size(30, 30)),
+        "assets/images/location.png");
     var myMarkers = places.map((place) => Marker(
-      icon: customIcon,
-      
-      infoWindow: InfoWindow(title: place.name),
-        markerId: MarkerId(place.id.toString()), position: place.latLng));
+        icon: customIcon,
+        infoWindow: InfoWindow(title: place.name),
+        markerId: MarkerId(place.id.toString()),
+        position: place.latLng));
     markers.addAll(myMarkers);
+  }
+
+  void initPolylines() {
+    Polyline p = const Polyline(polylineId: PolylineId("1"), points: [
+      LatLng(30.527345318683327, 31.14296885555196),
+      LatLng(30.548340141854784, 31.128549300603158),
+      LatLng(30.55321857050778, 31.033105579751563),
+      LatLng(322.12994090068492, 36.642809897210775),
+    ]);
+    polyLines.add(p);
+  }
+
+  void initPolygons() {
+    Polygon polygon = Polygon(
+      holes: [
+        [
+          LatLng(29.874758138985083, 27.46879482060436),
+          LatLng(29.960265827886758, 32.57413349509317),
+          LatLng(25.864308621765367, 31.390286845936345),
+        ]
+      ],
+      polygonId: const PolygonId("1"),
+      fillColor: Colors.black.withOpacity(.8),
+      strokeWidth: 3,
+      points: const [
+        LatLng(31.66331817316294, 25.14127848024088),
+        LatLng(31.17273472409815, 29.911141515268266),
+        LatLng(23.940583046127674, 35.0822712584423),
+        LatLng(22.067075678781233, 25.04054428135799),
+      ],
+    );
+    polyGons.add(polygon);
   }
 
   // void initMapStyle() async {
